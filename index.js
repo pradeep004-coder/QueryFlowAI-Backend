8cbc8182f20141dfdc317763ba2af41126fa1a79
pradeep <pp0931275@gmail.com>
2025-09-01 21:51:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//index.js
+import express from 'express';
+import cors from "cors" //to allow server access from different ports
+import dotenv from "dotenv";
+dotenv.config(); 
+import AuthRouter from "./Routes/AuthRouter.js"
+import ChatRouter from './Routes/ChatRouter.js';
+import connectDB from './Models/db.js'
+const app = express();
+const Port = process.env.PORT;
+
+connectDB();
+
+app.use(express.json()); // replaces body-parser.json()
+app.use(express.urlencoded({ extended: true })); // replaces body-parser.urlencoded()
+app.use(cors());
+app.use('/', AuthRouter);
+app.use('/', ChatRouter);
+
+app.listen(Port);
