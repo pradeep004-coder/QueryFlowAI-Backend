8a8099c5e0449252033ed0889d07c76a4092a139
pradeep <pp0931275@gmail.com>
2025-09-02 18:14:00 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import AuthRouter from "./Routes/AuthRouter.js"
 import ChatRouter from './Routes/ChatRouter.js';
 import connectDB from './Models/db.js'
 const app = express();
-// const Port = process.env.PORT;
+const Port = process.env.PORT;
 
 connectDB();
 
@@ -17,4 +17,4 @@ app.use(cors());
 app.use('/', AuthRouter);
 app.use('/', ChatRouter);
 
-// app.listen(Port);
+app.listen(Port);
